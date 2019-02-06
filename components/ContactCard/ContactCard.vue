3ef5f97bf7d4bc8c114f8a75612fa334f3307c92
Marcos Leonel <marcos@zeonsolutions.com.br>
2019-02-06 17:50:29 -0200

    feat:(component) adicao do mapa no card de contato

--- components/ContactCard/ContactCard.vue
+++ components/ContactCard/ContactCard.vue
@@ -1,6 +1,7 @@
 <template>
   <section :class="toggleCard ? 'contact-card--collapsed' : 'contact-card'">
 
+    <!-- INFORMAÇÕES PRIMÁRIAS -->
     <div class="row">
       <a @click="toggleCard = !toggleCard">
         <div class="contact-card__initial">
@@ -10,14 +11,16 @@
         </div>
       </a>
 
-      <div class="contact-card__names">
-        <h4 class="contact-card__names__civil-name">
-          {{ contactName }}
-        </h4>
-        <small class="contact-card__names__username">
-          {{ contactUsername }}
-        </small>
-      </div>
+      <a @click="toggleCard = !toggleCard">
+        <div class="contact-card__names">
+          <h4 class="contact-card__names__civil-name">
+            {{ contactName }}
+          </h4>
+          <small class="contact-card__names__username">
+            {{ contactUsername }}
+          </small>
+        </div>
+      </a>
 
       <div class="contact-card__primary-info">
         <p class="contact-card__primary-info__phone">
@@ -31,6 +34,27 @@
             icon="envelope"
             class="contact-card__primary-info__icon" />
           {{ contactEmail }}
+        </p>
+      </div>
+    </div>
+
+    <!-- INFORMAÇÕES SECUNDÁRIAS -->
+    <div :class="toggleCard ? 'row' : 'row--hidden'">
+      <div :class="toggleCard ? 'contact-card__map' : 'contact-card__map--hidden'">
+        <iframe
+          width="320"
+          height="160"
+          frameborder="0"
+          :src="`https://www.bing.com/maps/embed?h=160&w=320&cp=${contactGeoLat}~${contactGeoLat}&lvl=14&typ=s&sty=h&src=SHELL&FORM=MBEDV8`"
+          scrolling="no" />
+      </div>
+
+      <div class="contact-card__secondary-info">
+        <p class="contact-card__secondary-info__address">
+          <font-awesome-icon
+            icon="home"
+            class="contact-card__secondary-info__icon" />
+          {{ contactStreet }}, {{ contactSuite }}
         </p>
       </div>
     </div>
