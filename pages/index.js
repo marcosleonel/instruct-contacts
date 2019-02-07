849e90d66103dd5af32936bf3a1919dcbe4044ee
Marcos Leonel <leonel.coder@gmail.com>
2019-02-07 01:29:04 -0200

    feat:(component) melhorias feitas no header

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,44 @@ const App = {
 
   data: () => ({
     contactList: API_MOCK,
+    filteredList: API_MOCK,
+    filter: '',
   }),
+
+  watch: {
+    filter(val) {
+      this.filterList(this.contactList, val);
+    },
+  },
+
+  methods: {
+    filterList(list, filter) {
+      const filteredContacts = [];
+
+      for (let i = 0; i < list.length; i + 1) {
+        const contact = list[i];
+        const domain = contact.email.split('@').pop();
+
+        if (domain.indexOf(filter) !== -1) filteredContacts.push(contact);
+      }
+
+      this.filteredList = filteredContacts;
+    },
+
+    getFilterOptions(list) {
+      const options = [];
+
+      for (let i = 0; i < list.length; i++) { // eslint-disable-line
+        const contact = list[i];
+        const domain = contact.email.split('@').pop();
+        const option = domain.split('.').pop();
+
+        options.push(option);
+      }
+
+      return options;
+    },
+  },
 };
 
 export default App;
